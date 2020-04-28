54a6796272a3cb68d894de72629ff0259d438dad
Omar Antolín <omar.antolin@gmail.com>
2020-04-28 14:52:39 -0500

    Try to fix match data clobbering (& use better names)

--- my-lisp/live-completions.el
+++ my-lisp/live-completions.el
@@ -1,19 +1,19 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun update-completions (_start _end _length)
-  (minibuffer-completion-help))
+(defun live-completions--update (&optional _start _end _length)
+  (while-no-input (save-match-data (minibuffer-completion-help))))
 
-(defun live-completions-setup ()
+(defun live-completions--setup ()
   (make-local-variable 'after-change-functions)
-  (add-to-list 'after-change-functions #'update-completions)
-  (minibuffer-completion-help))
+  (add-to-list 'after-change-functions #'live-completions--update)
+  (live-completions--update))
 
 (define-minor-mode live-completions-mode
   "Live updating of the *Completions* buffer."
   :global t
   (if live-completions-mode
-      (add-hook 'minibuffer-setup-hook #'live-completions-setup)
-    (remove-hook 'minibuffer-setup-hook #'live-completions-setup)
+      (add-hook 'minibuffer-setup-hook #'live-completions--setup)
+    (remove-hook 'minibuffer-setup-hook #'live-completions--setup)
     (dolist (buffer (buffer-list))
       (when (minibufferp buffer)
         (with-current-buffer buffer
