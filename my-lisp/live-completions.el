eb021a9bcacea9a60f3fff99c1d7d6fc173caf85
Omar Antolín <omar.antolin@gmail.com>
2020-04-30 00:05:01 -0500

    Detect if we are completing

--- my-lisp/live-completions.el
+++ my-lisp/live-completions.el
@@ -1,19 +1,19 @@
 ;;; -*- lexical-binding: t; -*-
 
 (defun live-completions--update (&optional _start _end _length)
-  (save-match-data (while-no-input (minibuffer-completion-help))))
+  (when minibuffer-completion-table
+    (save-match-data (while-no-input (minibuffer-completion-help)))))
 
 (defconst live-completions--atomic-change-commmands
   '(choose-completion
     minibuffer-complete
     minibuffer-complete-and-exit
     minibuffer-force-complete
-    minibuffer-force-complete-and-exit
-    undo)
+    minibuffer-force-complete-and-exit)
   "List of commands whose changes should be atomic.")
 
 (defun live-completions--atomic-changes (fn &rest args)
-  (combine-after-change-calls (apply fn args)))
+  (let ((inhibit-modification-hooks t)) (apply fn args)))
 
 (defun live-completions--setup ()
   (live-completions--update)
@@ -32,8 +32,8 @@
       (advice-remove cmd #'live-completions--atomic-changes))
     (dolist (buffer (buffer-list))
       (when (minibufferp buffer)
-        (with-current-buffer buffer
-          (setq after-change-functions
-                (remove #'live-completions--update after-change-functions)))))))
+        (setf (buffer-local-value after-change-functions buffer)
+              (remove #'live-completions--update
+                      (buffer-local-value after-change-functions buffer)))))))
 
 (provide 'live-completions)
