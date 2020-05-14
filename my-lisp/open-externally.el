3c2ebcb0090cbb80299243ef2b52357e471c5111
Omar Antolín <omar.antolin@gmail.com>
2020-05-13 22:15:00 -0500

    Simplify open-externally

--- my-lisp/open-externally.el
+++ my-lisp/open-externally.el
@@ -6,14 +6,10 @@
   (if (and (eq system-type 'windows-nt)
            (fboundp 'w32-shell-execute))
       (w32-shell-execute "open" x)
-    (start-process-shell-command
-     "xdg-open" nil
-     (format "setsid -w xdg-open %s"
-             (shell-quote-argument
-              (expand-file-name x))))))
+    (call-process "xdg-open" nil 0 nil x)))
 
 (defun dired-open-externally (&optional arg)
-  "Open marked or current line's file in operating system's default application."
+  "Open marked or current file in operating system's default application."
   (interactive "P")
   (dired-map-over-marks
    (open-externally (dired-get-filename))
