732261221cd2fe1e49b822d67a7334a115d07cdb
Omar Antolín <omar.antolin@gmail.com>
2020-05-24 13:38:09 -0500

    Add smart case matching

--- my-lisp/regexpect.el
+++ my-lisp/regexpect.el
@@ -9,13 +9,32 @@
   :type 'function
   :group 'regexpect)
 
+(defcustom regexpect-smart-case t
+  "Whether to use smart case.
+If this variable is t, then case-sensitivity is decided as
+follows: if any component contains upper case letters, the
+matches are case sensitive; otherwise case-insensitive.  This
+like the behavior of `isearch' when `search-upper-case' is
+non-nil.
+
+On the other hand, if this variable is nil, then case-sensitivity
+is determined by the values of `completion-ignore-case',
+`read-file-name-completion-ignore-case' and
+`read-buffer-completion-ignore-case', as usual for completion."
+  :type 'boolean
+  :group 'regexpect)
+
 (defun regexpect--internal (string table &optional pred)
   (save-match-data
     (let* ((limit (car (completion-boundaries string table pred "")))
            (prefix (substring string 0 limit))
            (pattern (substring string limit))
            (regexp (funcall regexpect-converter pattern))
-           (completion-regexp-list (list regexp)))
+           (completion-regexp-list (list regexp))
+           (completion-ignore-case
+            (if regexpect-smart-case
+                (isearch-no-upper-case-p regexp t)
+              completion-ignore-case)))
       (list
        (condition-case nil
            (all-completions prefix table pred)
@@ -27,7 +46,7 @@
   "Convert STRING to a regexp and find entries TABLE matching it all.
 The predicate PRED is used to constrain the entries in TABLE.  The
 matching portions of each candidate are highlighted.
-This function is part of the `regexp' completion style."
+This function is part of the `regexpect' completion style."
   (cl-destructuring-bind (completions regexp prefix)
       (regexpect--internal string table pred)
     (when completions
@@ -53,14 +72,16 @@ STRING in TABLE among entries satisfying PRED.  If there is only
 one match, it completes to that match.  If there are no matches,
 it returns nil.  In any other case it \"completes\" STRING to
 itself, without moving POINT.  This function is part of the
-`orderless' completion style."
-  (cl-destructuring-bind (completions _regexp prefix)
+`regexpect' completion style."
+  (cl-destructuring-bind (completions regexp prefix)
       (regexpect--internal string table pred)
     (cond
      ((null completions) nil)
      ((null (cdr completions))
       (let ((full (concat prefix (car completions))))
         (cons full (length full))))
+     ((string= regexp (regexp-quote regexp))
+      (completion-substring-try-completion string table pred point))
      (t (cons string point)))))
 
 (add-to-list 'completion-styles-alist
