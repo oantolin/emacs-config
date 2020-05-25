05d93945f4b561f0e14726e176c35e40df99656d
Omar Antolín <omar.antolin@gmail.com>
2020-05-25 15:50:43 -0500

    Add other OSes (without testing :P), use absolute paths

--- my-lisp/open-externally.el
+++ my-lisp/open-externally.el
@@ -1,12 +1,17 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun open-externally (x)
+(defun open-externally (file)
   "Open file using system's default application."
   (interactive "fOpen: ")
   (if (and (eq system-type 'windows-nt)
            (fboundp 'w32-shell-execute))
-      (w32-shell-execute "open" x)
-    (call-process "xdg-open" nil 0 nil x)))
+      (w32-shell-execute "open" (expand-file-name file))
+    (call-process (pcase system-type
+                    ('darwin "open")
+                    ('cygwin "cygstart")
+                    (_ "xdg-open"))
+                  nil 0 nil
+                  (expand-file-name file))))
 
 (defun dired-open-externally (&optional arg)
   "Open marked or current file in operating system's default application."
