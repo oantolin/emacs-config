b0276ee0168e01f3e7b0d865d6f51341b4d51300
Omar Antolín <omar.antolin@gmail.com>
2020-09-13 13:33:01 -0500

    Try out whole-line-or-region mode

--- init.el
+++ init.el
@@ -239,6 +239,11 @@
   (isearch-mode-end . isearch-exit-at-start)
   (isearch-mode . isearch-yank-region))
 
+(use-package whole-line-or-region
+  :ensure t
+  :diminish whole-line-or-region-local-mode
+  :config (whole-line-or-region-global-mode))
+
 (use-package math-delimiters
   :load-path "~/my-elisp-packages/math-delimiters"
   :bind
