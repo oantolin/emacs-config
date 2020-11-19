3f8601456454e5202cbc08ffedf7f3a0d97d70a5
Omar Antolín <omar.antolin@gmail.com>
2020-11-19 10:09:46 -0600

    Add case where tmp-buffer prompts for mode

--- my-lisp/tmp-buffer.el
+++ my-lisp/tmp-buffer.el
@@ -6,10 +6,24 @@
     (?m . markdown-mode)
     (?l . lisp-interaction-mode)
     (?x . LaTeX-mode)
-    (?f . fundamental-mode))
+    (?f . fundamental-mode)
+    (?? . tmp-buffer-prompt-for-mode))
   "List of major modes for temporary buffers and their hotkeys."
   :type '(alist :key-type character :value-type symbol)
   :group 'tmp-buffer)
+
+(defvar tmp-buffer-mode-history nil)
+
+(defun tmp-buffer-prompt-for-mode ()
+  "Prompt for a major mode and switch to it."
+  (funcall
+   (intern
+    (completing-read
+     "Mode: "
+     (cl-loop for m being the symbols
+              when (and (commandp m) (string-suffix-p "-mode" (symbol-name m)))
+              collect m)
+     nil t nil 'tmp-buffer-mode-history))))
 
 (defun tmp-buffer (spec)
   "Open temporary buffer in specified major mode."
