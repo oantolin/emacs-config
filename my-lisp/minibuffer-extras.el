963c2e2c1e968ced0be185aa97b093ea6c855e6d
Omar Antolín <omar.antolin@gmail.com>
2020-12-06 17:41:48 -0600

    Remove functions superceded by consult
    consult-completing-yank is not visually as nice as my function, but
    I'll endeavor to improve the one in consult instead of using mine.
    
    Also, I need to make consult-line and consult-yank-* start in list
    mode in embark occur. That can be done with advice, maybe that's the
    way to go for now.

--- my-lisp/minibuffer-extras.el
+++ my-lisp/minibuffer-extras.el
@@ -1,14 +1,5 @@
 ;;; -*- lexical-binding: t; -*-
 
-;;; switching to completions
-
-(defun right-char-or-completions ()
-  "Move to right if possible, otherwise switch to completions buffer."
-  (interactive)
-  (if (= (point) (point-max))
-      (switch-to-completions)
-    (right-char)))
-
 ;;; tools for navigating the file system in the minibuffer
 ;;; bind in minibuffer-local-filename-map
 
@@ -38,30 +29,6 @@
   (when (minibufferp)
     (delete-region (minibuffer-prompt-end) (point-max)))
   (insert (cdr (assoc bm (cdb--bookmarked-directories)))))
-
-;;; use minibuffer for in-buffer completion
-;;; set as completion-in-region-function
-
-(defun completing-read-in-region (start end collection &optional predicate)
-  "Prompt for completion of region in the minibuffer if non-unique.
-Use as a value for `completion-in-region-function'."
-  (if (and (minibufferp) (not (string= (minibuffer-prompt) "Eval: ")))
-      (completion--in-region start end collection predicate)
-    (let* ((initial (buffer-substring-no-properties start end))
-           (limit (car (completion-boundaries initial collection predicate "")))
-           (all (completion-all-completions initial collection predicate
-                                            (length initial)))
-           (completion (cond
-                        ((atom all) nil)
-                        ((and (consp all) (atom (cdr all)))
-                         (concat (substring initial 0 limit) (car all)))
-                        (t (completing-read
-                            "Completion: " collection predicate t initial)))))
-      (if (null completion)
-          (progn (message "No completion") nil)
-        (delete-region start end)
-        (insert completion)
-        t))))
 
 (defun minibuffer-try-complete-and-exit ()
     (interactive)
