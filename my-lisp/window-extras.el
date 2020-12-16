4ea5cd8b0c2130cd679410b2a8f977ad5ca83f49
Omar Antolín <omar.antolin@gmail.com>
2020-12-16 17:37:12 -0600

    Add alphapapa's screenshot function using x-export-frames

--- my-lisp/window-extras.el
+++ my-lisp/window-extras.el
@@ -23,4 +23,16 @@
     (set-window-buffer next-window this-buffer)
     (select-window next-window)))
 
+;; Written by alphapapa, https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/g2c2c6y
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 (provide 'window-extras)
