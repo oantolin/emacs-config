f3d0c49add61a4f9686c37681ef84b856ce076ae
Omar Antolín <omar.antolin@gmail.com>
2020-12-23 13:19:59 -0600

    Bind even more consult commands

--- init.el
+++ init.el
@@ -440,19 +440,21 @@
 (use-package consult
   :ensure t
   :bind
-  ("C-M-y" . consult-yank-replace)
-  ("M-g l" . consult-line)    ("M-g M-l" . consult-line)
-  ("M-g i" . consult-imenu)   ("M-g M-i" . consult-imenu)
-  ("M-g o" . consult-outline) ("M-g M-o" . consult-outline)
+  ("M-y" . consult-yank-pop)
+  ("M-g l" . consult-line)        ("M-g M-l" . consult-line)
+  ("M-g i" . consult-imenu)       ("M-g M-i" . consult-imenu)
+  ("M-g o" . consult-outline)     ("M-g M-o" . consult-outline)
+  ("M-g m" . consult-mark)        ("M-g M-m" . consult-mark)
+  ("M-g k" . consult-global-mark) ("M-g M-k" . consult-global-mark)
+  ("M-g e" . consult-error)       ("M-g M-e" . consult-error)
   ("M-g ." . consult-line-symbol-at-point)
   ("M-s l" . consult-line-from-isearch)
   ("M-s m" . consult-multi-occur)
+  ("M-X" . consult-mode-command)
+  ("C-c b" . consult-buffer)
   (:map minibuffer-local-map
         ("M-r" . consult-history)
         ("M-s"))
-  :commands
-  consult-completion-in-region
-  consult-file-externally
   :custom
   (completion-in-region-function #'consult-completion-in-region)
   :config
