9da4d80f35083c3412cc63a54f89b691d44c852a
Omar Antolín <omar.antolin@gmail.com>
2021-01-11 20:00:56 -0600

    Add yet another completion setup: the one I sent to Augusto

--- my-lisp/various-toggles.el
+++ my-lisp/various-toggles.el
@@ -19,21 +19,34 @@
      ("english" "español")
      ("español" "english"))))
 
+(defun embark-collect-completions-1 (_start _end)
+  (embark-collect-completions))
+
 (defun change-completion-ui ()
   "Choose between Embark, Icomplete and Selectrum for completion."
   (interactive)
   (icomplete-mode -1)
   (remove-hook 'minibuffer-setup-hook #'embark-collect-completions-after-input)
   (remove-hook 'minibuffer-setup-hook #'embark-collect-completions-after-delay)
+  (defalias 'minibuffer-completion-help 'original-minibuffer-completion-help)
+  (defalias 'switch-to-completions 'original-switch-to-completions)
+  (setq completion-auto-help nil)
   (selectrum-mode -1)
-  (let ((ui (read-char-choice "Default, Embark, Icomplete or Selectrum? "
-                              '(?d ?i ?e ?E ?s))))
+  (let ((ui (read-char-choice
+             "[d]efault, Embark ([e]asy, [I]nput, [D]elay), [i]complete or [s]electrum? "
+             '(?d ?D ?i ?I ?e ?s))))
     (pcase ui
-      ((or ?e ?E)
+      ((or ?I ?D)
        (add-hook 'minibuffer-setup-hook
-                 (if (= ui ?e)
+                 (if (= ui ?I)
                      #'embark-collect-completions-after-input
                    #'embark-collect-completions-after-delay)))
+      (?e
+       (setq completion-auto-help t)
+       (advice-add 'minibuffer-completion-help
+                   :override 'embark-collect-completions-1)
+       (advice-add 'switch-to-completions
+                   :override 'embark-switch-to-collect-completions))
       (?i (icomplete-mode))
       (?s (selectrum-mode)))))
 
