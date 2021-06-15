1a31c7a806a83b9f4fb802f86671d9866a920b60
Omar AntolÃ­n Camarena <omar.antolin@gmail.com>
2021-06-14 21:51:33 -0500

    Collect my org extras from various parts here
    1. Add :export property for doi links (:follow comes with org).
    2. Add arXiv links with useable :follow and :export
    3. Move inline-js "language" here from private config.
    4. Move latex-macros "language" here from private config.

--- my-lisp/org-extras.el
+++ my-lisp/org-extras.el
@@ -1,3 +1,71 @@
 ;;; -*- lexical-binding: t; -*-
 
+;;; arXiv links
+
+(org-link-set-parameters
+ "arXiv"
+ :face 'org-link
+ :follow (lambda (path)
+           (when (string-match "^\\(.*\\)/\\(.*\\)$" path)
+             (browse-url
+              (format "http://arxiv.org/abs/%s"
+                      (match-string 1 path)))))
+ :export (lambda (path desc backend)
+           (when (and (memq backend '(latex html))
+                      (null desc)
+                      (string-match "^\\(.*\\)/\\(.*\\)$" path))
+             (format
+              (pcase backend
+                ('latex "\\href{https://arxiv.org/abs/%s}{\\texttt{arXiv:%s [%s]}}")
+                ('html "<a href=\"https://arxiv.org/abs/%s\"><code>arXiv:%s [%s]</code></a>"))
+              (match-string 1 path)
+              (match-string 1 path)
+              (match-string 2 path)))))
+
+;;; doi links
+
+(org-link-set-parameters
+ "doi" ; already has a sensible :follow
+ :export (lambda (path desc backend)
+           (when (and (memq backend '(latex html))
+                      (null desc))
+             (format
+              (pcase backend
+                ('latex "\\href{https://doi.org/%s}{\\texttt{doi:%s}}")
+                ('html "<a href=\"https://doi.org/%s\"><code>doi:%s</code></a>"))
+              path
+              path))))
+
+;;; Inline JavaScript
+
+(add-to-list 'org-src-lang-modes '("inline-js" . javascript))
+
+(defvar org-babel-default-header-args:inline-js
+  '((:results . "html")
+    (:exports . "results")))
+
+(defun org-babel-execute:inline-js (body _params)
+  (format "<script type=\"text/javascript\">\n%s\n</script>" body))
+
+;;; Source blocks for declaring LaTeX macros
+
+(add-to-list 'org-src-lang-modes '("latex-macros" . latex))
+
+(defvar org-babel-default-header-args:latex-macros
+  '((:results . "raw")
+    (:exports . "results")))
+
+(defun prefix-all-lines (pre body)
+  (with-temp-buffer
+    (insert body)
+    (string-insert-rectangle (point-min) (point-max) pre)
+    (buffer-string)))
+
+(defun org-babel-execute:latex-macros (body _params)
+  (concat
+   (prefix-all-lines "#+LATEX_HEADER: " body)
+   "\n#+HTML_HEAD_EXTRA: <div style=\"display: none\"> \\(\n"
+   (prefix-all-lines "#+HTML_HEAD_EXTRA: " body)
+   "\n#+HTML_HEAD_EXTRA: \\)</div>\n"))
+
 (provide 'org-extras)
