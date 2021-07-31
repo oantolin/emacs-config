775c48fd8caddc400d7f21355ae6d1ffd2189e39
Omar Antolín <omar.antolin@gmail.com>
2021-07-31 02:17:27 -0500

    Try the default mixed indicator for a while

--- init.el
+++ init.el
@@ -423,7 +423,6 @@
   (embark-collect-post-revert . resize-embark-collect-completions)
   :custom
   (embark-quit-after-action nil)
-  (embark-indicator #'embark-minimal-indicator)
   (prefix-help-command #'embark-prefix-help-command)
   :custom-face
   (embark-keybinding
