6dfc169aaad985e692e77fcb8ff80de4f7743a1f
Omar Antolín <omar.antolin@gmail.com>
2021-07-31 01:59:26 -0500

    Update which-key indicator

--- my-lisp/embark-which-key-indicator.el
+++ my-lisp/embark-which-key-indicator.el
@@ -2,23 +2,22 @@
 
 (require 'which-key)
 
-(defun embark-which-key-indicator (keymap targets)
-  "An embark indicator that displays KEYMAP with which-key.
+(defun embark-which-key-indicator ()
+  "An embark indicator that displays keymaps using which-key.
 The which-key help message will show the type and value of the
 current target followed by an ellipsis if there are further
-TARGETS."
-  (which-key--show-keymap
-   (if (eq (caar targets) 'embark-become)
-       "Become"
-     (format "Act on %s '%s'%s"
-             (caar targets)
-             (embark--truncate-target (cdar targets))
-             (if (cdr targets) "…" "")))
-   keymap
-   nil nil t)
-  (lambda (prefix)
-    (if prefix
-        (embark-which-key-indicator (lookup-key keymap prefix) targets)
-      (kill-buffer which-key--buffer))))
+targets."
+  (lambda (&optional keymap targets prefix)
+    (if (null keymap)
+        (kill-buffer which-key--buffer)
+      (which-key--show-keymap
+       (if (eq (caar targets) 'embark-become)
+           "Become"
+         (format "Act on %s '%s'%s"
+                 (caar targets)
+                 (embark--truncate-target (cdar targets))
+                 (if (cdr targets) "…" "")))
+       (if prefix (lookup-key keymap prefix) keymap)
+       nil nil t))))
 
 (provide 'embark-which-key-indicator)
