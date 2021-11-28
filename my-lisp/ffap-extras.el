c172aedb58eb1ec8a1c64f53f561b43d27765630
Omar Antolín <omar.antolin@gmail.com>
2021-11-28 11:45:51 -0600

    Better directory guessing for ffap in eshell buffers
    In eshell buffers, by default ffap only looks for files in the current
    default-directory. This little library has it also look for the
    directory heading in a multi-directory ls listing, and for a directory
    at the end of the command that listed the file.
    
    These heuristics are not perfect, but they find the right file in more
    cases ---and in the process make Embark file actions much more useful
    in eshell buffers.

---
+++ my-lisp/ffap-extras.el
@@ -0,0 +1,28 @@
+;;; ffap-extras.el --- Extra rules for ffap    -*- lexical-binding: t; -*-
+
+;;; Improved directory guessing in eshell buffers:
+
+(defun guess-directory-from-face ()
+  (save-excursion
+    (backward-paragraph)
+    (forward-char)
+    (when (eq (get-text-property (point) 'face) 'eshell-ls-directory)
+      (buffer-substring-no-properties
+       (point) (next-single-property-change (point) 'face)))))
+
+(defun guess-directory-from-prompt ()
+  (save-excursion
+    (eshell-previous-prompt 1)
+    (end-of-line)
+    (thing-at-point 'filename)))
+
+(defun ffap-eshell-mode (name)
+  (seq-find #'file-exists-p
+            (mapcar (lambda (dir) (expand-file-name name dir))
+                    (delq nil (list default-directory
+                                    (guess-directory-from-face)
+                                    (guess-directory-from-prompt))))))
+
+(setf (alist-get 'eshell-mode ffap-alist) #'ffap-eshell-mode)
+
+(provide 'ffap-extras)
