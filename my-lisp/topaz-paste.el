05af3f35db01e84f8d0ea52cfde7976211968ff0
Omar Antolín <omar.antolin@gmail.com>
2021-12-02 08:27:30 -0600

    New library to share text using https://topaz.github.io/paste/
    https://topaz.github.io/paste/ is a "client side paste service", which
    can be used to share snippets of text. All the data is encoded in the
    URL as a Base64-encoded LZMA-compressed string.

---
+++ my-lisp/topaz-paste.el
@@ -0,0 +1,23 @@
+;;; topaz-paste.el --- Generate topaz paste URLs    -*- lexical-binding: t; -*-
+
+;; https://topaz.github.io/paste/ is a "client side paste service",
+;; which can be used to share snippets of text. All the data is
+;; encoded in the URL as a Base64-encoded LZMA-compressed string.
+;; Since both Base64 and LZMA are pretty standard this means you don't
+;; actually need the website to generate a URL!
+
+(defun topaz-paste-region (begin end)
+  "Save topaz paste URL for region contents on the kill-ring."
+  (interactive "r")
+  (kill-new
+   (concat "https://topaz.github.io/paste/#"
+           (with-output-to-string
+             (call-shell-region begin end "xz -zc --format=lzma | base64"
+                                nil standard-output)))))
+
+(defun topaz-paste-buffer ()
+  "Save topaz paste URL for buffer contents on the kill-ring."
+  (interactive)
+  (topaz-paste-region (point-min) (point-max)))
+
+(provide 'topaz-paste)
