5aa0a57781a8e731d26fc279952ad8bdd1a81cbe
Omar Antolín <omar.antolin@gmail.com>
2022-01-29 11:39:57 -0600

    Bind w to copy URL in pocket-reader
    The default binding is c which ironically I find very hard to remember!

--- init.el
+++ init.el
@@ -1145,10 +1145,15 @@ Use this function as :filter-args advice for `org-gnus-article-link'."
 (use-package pocket-reader
   :ensure t
   :defer t
+  :bind
+  (:map pocket-reader-mode-map
+        ("w" . pocket-reader-copy-url)) ; the default binding of c is "unemacsy"
   :custom
   (pocket-reader-open-url-default-function #'eww)
   (pocket-reader-pop-to-url-default-function #'eww)
   :config
+  ;; I don't need this target finder now, but someday I might add some
+  ;; URL actions that aren't covered by Pocket Reader commands.
   (defun target-pocket-reader-url ()
     "Target the URL of the pocket-reader item at point."
     (when-let (((derived-mode-p 'pocket-reader-mode))
