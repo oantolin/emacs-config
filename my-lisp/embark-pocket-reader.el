cbe2efa711ec52bb786e06fcf65b7ac0d9ea4065
Omar AntolÃ­n Camarena <omar.antolin@gmail.com>
2022-02-27 12:37:06 -0600

    Add message about adding URLs to Pocket

--- my-lisp/embark-pocket-reader.el
+++ my-lisp/embark-pocket-reader.el
@@ -1,4 +1,4 @@
-;;; embark-pocket-reader.el --- Target URLs in pocket-reader   -*- lexical-binding: t; -*-
+;;; embark-pocket-reader.el --- Embark integration for pocket-reader   -*- lexical-binding: t; -*-
 
 (require 'pocket-reader)
 (require 'embark)
@@ -11,6 +11,12 @@
              (url (pocket-reader--get-url item)))
     `(url ,url ,(line-beginning-position) . ,(line-end-position))))
 
+(cl-defun embark-pocket-reader--added (&key target &allow-other-keys)
+  (message "Added: %s" target))
+
 (add-to-list 'embark-target-finders #'embark-pocket-reader-target-url)
+
+(add-to-list 'embark-post-action-hooks
+             '(pocket-lib-add-urls embark-pocket-reader--added))
 
 (provide 'embark-pocket-reader)
