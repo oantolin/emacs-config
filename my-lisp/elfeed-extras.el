4527b02fd9f9dd8d294cb2db95280c27d698ec98
Omar AntolÃ­n Camarena <omar.antolin@gmail.com>
2022-03-01 01:11:20 -0600

    Fix typo, refactor

--- my-lisp/elfeed-extras.el
+++ my-lisp/elfeed-extras.el
@@ -2,9 +2,17 @@
 
 (require 'elfeed)
 
+(defun elfeed-extras--entry ()
+  "Return the current entry in either Elfeed search or show mode."
+  (or (elfeed-search-selected t) elfeed-show-entry))
+
+(defun elfeed-extras--browse (url generic)
+  "Browse URL with `browse-url' or, if GENERIC, with `browse-url-generic'."
+  (if generic (browse-url-generic url) (browse-url url)))
+
 (defun elfeed-extras--get-link-by-title (title)
   "Return the link in entry contents with TITLE, if any."
-  (when-let ((entry (or (elfeed-search-selected t) elfeed-show-entry))
+  (when-let ((entry (elfeed-extras--entry))
              (content (elfeed-deref (elfeed-entry-content entry))))
     (when (string-match
            (format "<a href=\"\\([^\"]+\\)\">[^<]*%s[^<]*</a>" title)
@@ -15,22 +23,22 @@
   "Browse comments in current entry."
   (interactive "P")
   (when-let ((url (elfeed-extras--get-link-by-title "comments")))
-    (if generic (browse-url-generic url) (browse-url url))))
+    (elfeed-extras--browse url generic)))
 
 (defun elfeed-extras-link (&optional generic)
   "Browse link named 'link' in current entry."
   (interactive "P")
   (when-let ((url (elfeed-extras--get-link-by-title "link")))
-    (if generic (browse-url-generic url) (browse-url url))))
+    (elfeed-extras--browse url generic)))
 
 (defun elfeed-extras-arxiv-pdf (&optional generic)
   "View the PDF corresponing to an arXiv submission in Elfeed."
   (interactive "P")
   (when-let ((abs "^https?://arxiv.org/abs/\\(.*\\)$")
-             (pdf "https://arxiv.org/pdf/%s.pdf")
-             (item (or (elfeed-search-selected t) elfeed-show-entry))
-             (url (elfeed-entry-link item))
-             ((string-match abs url)))
-    (if generic (browse-url-generic url) (browse-url url))))
+             (entry (elfeed-extras--entry))
+             (url (elfeed-entry-link entry))
+             ((string-match abs url))
+             (pdf (format "https://arxiv.org/pdf/%s.pdf" (match-string 1 url))))
+    (elfeed-extras--browse pdf generic)))
 
 (provide 'elfeed-extras)
