42bf02046ecce5f795659163347c7a73232a1a0a
Omar Antolín <omar.antolin@gmail.com>
2022-03-30 16:03:56 -0600

    Bluejeans should go to the external browser too

--- init.el
+++ init.el
@@ -771,7 +771,8 @@ default."
    '(("\\`https?://\\(?:youtu\\.be\\|\\(?:www\\.\\)youtube\\.com\\)"
       . browse-url-default-browser)
      ("\\`https?://[^/]+zoom\\.us" . browse-url-default-browser)
-     ("https://meet\\.google\\.com" . browse-url-default-browser)))
+     ("https?://meet\\.google\\.com" . browse-url-default-browser)
+     ("https?://bluejeans\\.com" . browse-url-default-browser)))
   :config
   (advice-add 'browse-url-generic :override 'browse-url-default-browser))
 
