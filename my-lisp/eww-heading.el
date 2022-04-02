2b360bee8edf59243351eb31ccb25680842abf57
Omar Antolín <omar.antolin@gmail.com>
2022-04-02 10:19:16 -0600

    Simplify eww-heading-next using text-property-search-forward
    Thanks to @minad for pointing this function out.
    
    (I also fixed a couple of silly typos in this commit.)

--- my-lisp/eww-heading.el
+++ my-lisp/eww-heading.el
@@ -6,30 +6,25 @@ If ARG is negative move backwards, ARG defaults to 1."
   (interactive "p")
   (unless arg (setq arg 1))
   (catch 'return
-    (cl-flet ((headingp ()
-                (when-let ((face (get-text-property (point) 'face)))
-                  (and (consp face)
-                       (cl-intersection face '(shr-h1 shr-h2 shr-h3 shr-h4))))))
-      (dotimes (_ (abs arg))
-        (when (headingp)
-          (unless (zerop (forward-line (if (> arg 0) 1 -1)))
-            (throw 'return nil)))
-        (while (not (headingp))
-          (if-let ((find-change (if (> arg 0)
-                                    #'next-single-property-change
-                                  #'previous-single-property-change))
-                   (change (funcall find-change (point) 'face)))
-              (goto-char change)
-            (goto-char (if (> arg 0) (point-max) (point-min)))
-            (throw 'return nil))))
-      (beginning-of-line)
-      (point))))
+    (dotimes (_ (abs arg))
+      (when (> arg 0) (end-of-line))
+      (unless (funcall
+               (if (> arg 0)
+                   #'text-property-search-forward
+                 #'text-property-search-backward)
+               'face '(shr-h1 shr-h2 shr-h3 shr-h4)
+               (lambda (tags face)
+                 (cl-loop for x in (ensure-list face) thereis (memq x tags))))
+        (throw 'return nil))
+      (when (< arg 0) (beginning-of-line)))
+    (beginning-of-line)
+    (point)))
 
 (defun eww-heading-previous (&optional arg)
   "Move backward by ARG headings (any h1-h4).
 If ARG is negative move forwards instead, ARG defaults to 1."
   (interactive "p")
-  (eww-next-heading (- (or arg 1))))
+  (eww-heading-next (- (or arg 1))))
 
 (defun eww-heading--line-at-point ()
   "Return the current line."
@@ -39,7 +34,7 @@ If ARG is negative move forwards instead, ARG defaults to 1."
   "Setup imenu for h1-h4 headings in eww buffer.
 Add this function to `eww-mode-hook'."
   (setq-local
-   imenu-prev-index-position-function #'eww-previous-heading
-   imenu-extract-index-name-function  #'line-at-point))
+   imenu-prev-index-position-function #'eww-heading-previous
+   imenu-extract-index-name-function  #'eww-heading--line-at-point))
 
 (provide 'eww-heading)
