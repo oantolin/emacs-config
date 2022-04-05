1a4c1c9ae8b4071550e8bc3cfbe546a40167021e
Omar AntolÃ­n Camarena <omar.antolin@gmail.com>
2022-04-05 00:54:27 -0500

    beginend has been fixed for elfeed search buffers

--- init.el
+++ init.el
@@ -654,8 +654,6 @@ default."
   :ensure t
   :diminish beginend-global-mode
   :config
-  (beginend-define-mode elfeed-search-mode
-    (progn) (progn (forward-line -1)))
   (dolist (mode beginend-modes) (diminish (cdr mode)))
   (beginend-global-mode))
 
