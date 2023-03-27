3a851503cc0d6605d8789c3680b3d53ffccf4ee3
Omar Antolín Camarena <omar.antolin@gmail.com>
2023-03-27 12:49:52 -0600

    Whoops, missed a ; when changing the embark bindings

--- init.el
+++ init.el
@@ -470,7 +470,7 @@
   (embark-indicators '(embark-minimal-indicator
                        embark-highlight-indicator
                        embark-isearch-highlight-indicator))
-  (embark-cycle-key ";")
+  (embark-cycle-key ".")
   (embark-help-key "?")
   (embark-confirm-act-all nil)
   :config
