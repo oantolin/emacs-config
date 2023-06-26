faef63be97c060e2acff4ac8ff44ec868a6d5ba2
Omar Antolín Camarena <omar.antolin@gmail.com>
2023-06-25 21:11:12 -0500

    Add docstring to block-undo
    Also, don't apply advice to dot-mode-execute anymore.

--- my-lisp/block-undo.el
+++ my-lisp/block-undo.el
@@ -1,13 +1,13 @@
 ;;; block-undo.el --- Undo keyboard macros in a single step    -*- lexical-binding: t; -*-
 
 (defun block-undo (fn &rest args)
+  "Apply FN to ARGS in such a way that it can be undone in a single step."
   (let ((marker (prepare-change-group)))
     (unwind-protect (apply fn args)
       (undo-amalgamate-change-group marker))))
 
 (dolist (fn '(kmacro-call-macro
               kmacro-exec-ring-item
-              dot-mode-execute
               apply-macro-to-region-lines))
   (advice-add fn :around #'block-undo))
 
