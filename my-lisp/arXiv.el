20b1c69437bc5bb733e5d723cca00b05817d9a88
Omar Antolín <omar.antolin@gmail.com>
2023-08-22 22:08:11 -0600

    Command to capture an arXiv paper

--- my-lisp/arXiv.el
+++ my-lisp/arXiv.el
@@ -26,24 +26,55 @@ If GENERIC is non-nil use `url-browse-generic' rather than
 
 (defvar url-http-end-of-headers)
 
+(defun arXiv--with-metadata (paper fn)
+  "Download and parse arXiv metadata for PAPER.
+Call FN on an alist with keys `title', `authors', `url', `id' and
+`abstract'."
+  (url-retrieve
+   (format "http://export.arxiv.org/api/query?id_list=%s" (arXiv--id paper))
+   (lambda (_)
+     (goto-char url-http-end-of-headers)
+     (forward-line 2)
+     (let* ((xml (libxml-parse-xml-region (point)))
+            (entry (car (dom-by-tag xml 'entry)))
+            (title (dom-text (dom-by-tag entry 'title)))
+            (abstract (dom-text (dom-by-tag entry 'summary)))
+            (authors (mapcar #'dom-texts (dom-by-tag entry 'author)))
+            (url (dom-text (dom-by-tag entry 'id)))
+            (id (string-remove-prefix "http://arxiv.org/abs/" url)))
+       (funcall fn `((title . ,title) (authors . ,authors)
+                     (abstract . ,abstract) (url . ,url) (id . ,id)))))))
+
 (defun arXiv-show (paper)
   "Popup a buffer with the title, authors and abstract of PAPER.
 A ragtag bunch of ad hoc formats are recognized for PAPER."
   (interactive "sarXiv paper: ")
-  (let ((id (arXiv--id paper)))
-    (url-retrieve
-     (format "http://export.arxiv.org/api/query?id_list=%s" id)
-     (lambda (_)
-       (goto-char url-http-end-of-headers)
-       (forward-line 2)
-       (let* ((xml (libxml-parse-xml-region (point)))
-              (entry (car (dom-by-tag xml 'entry)))
-              (title (dom-text (dom-by-tag entry 'title)))
-              (abstract (dom-text (dom-by-tag entry 'summary)))
-              (authors (mapcar #'dom-texts (dom-by-tag entry 'author))))
-         (with-output-to-temp-buffer (format "*arXiv:%s*" id)
-           (princ
-            (format "%s\nBy: %s\n\n%s\n"
-                    title (string-join authors ", ") abstract))))))))
+  (arXiv--with-metadata
+   paper
+   (lambda (data)
+     (let-alist data
+       (with-output-to-temp-buffer (format "*arXiv:%s*" .id)
+         (princ (format "%s\nBy: %s\n\n%s\n"
+                        .title (string-join .authors ", ") .abstract)))))))
+
+(defun arXiv-capture (paper)
+  "Capture Org entry for arXiv PAPER at point.
+(This depends on the `org-capture' template 'a' just inserting the
+kill ring head. 😬)"
+  (interactive "sarXiv paper: ")
+  (arXiv--with-metadata
+   paper
+   (lambda (data)
+     (let-alist data
+       (kill-new (format "[[%s][%s]]\nBy: %s\n\n%s\n"
+                         .url .title (string-join .authors ", ") .abstract)))
+     (org-capture nil "a"))))
+
+(defvar-keymap arXiv-map
+  "p" #'arXiv-pdf
+  "s" #'arXiv-show
+  "c" #'arXiv-capture)
+
+(fset 'arXiv-map arXiv-map)
 
 (provide 'arXiv)
