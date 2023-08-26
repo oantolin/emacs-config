65ecd1fe13ca167d81d882a862fc4d31442bbcf1
Omar Antolín <omar.antolin@gmail.com>
2023-08-25 19:35:23 -0600

    Better way to set PATH and exec-path

--- init.el
+++ init.el
@@ -54,8 +54,8 @@
 (when (string= (system-name) "penguin") ; Chromebook
   (dolist (dir '("~/texlive/2022/bin/x86_64-linux" "~/bin"))
     (let ((full (expand-file-name dir)))
-      (setenv "PATH" (concat full ":" (getenv "PATH")))))
-  (setq exec-path (split-string (getenv "PATH") ":")))
+      (setenv "PATH" (concat full ":" (getenv "PATH")))
+      (push full exec-path))))
 
 (dolist (cmd '(narrow-to-region
                upcase-region
