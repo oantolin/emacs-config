0a1912c69c67327410b142b707e1ab170cc0ec67
Omar Antolín <omar.antolin@gmail.com>
2023-09-01 03:38:41 -0600

    Provide convenience keymap for candidate inserter

--- my-lisp/text-extras.el
+++ my-lisp/text-extras.el
@@ -242,6 +242,24 @@ and quit the window, killing the buffer."
 (autoload 'embark--act "embark")
 (autoload 'embark--targets "embark")
 
+(defvar-keymap insertion-map
+  :doc "Short cuts to common types of insertion commands"  
+  "/" #'find-file
+  "v" #'describe-variable
+  "f" #'describe-function
+  "x" #'execute-extended-command
+  "s" #'describe-symbol
+  "k" #'describe-keymap
+  "F" #'describe-face
+  "u" #'insert-char
+  "i" #'set-input-method
+  "m" #'man
+  "L" #'find-library
+  "p" #'describe-package
+  "b" #'switch-to-buffer
+  "l" 'consult-line
+  "e" 'compose-mail-to) 
+
 (defun insert-completion-candidate ()
   "Rewire next command so that RET inserts the candidate."
   (interactive)
@@ -256,6 +274,7 @@ and quit the window, killing the buffer."
                         (interactive)
                         (embark--act 'embark-insert (car (embark--targets)) t)))
               (current-local-map)))))
-    (add-hook 'minibuffer-setup-hook ret-inserts)))
+    (add-hook 'minibuffer-setup-hook ret-inserts)
+    (set-transient-map insertion-map)))
 
 (provide 'misc-text)
