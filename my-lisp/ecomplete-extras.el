956f691ce763f2a0d661a16754fae6764c5528a6
Omar Antolín <omar.antolin@gmail.com>
2023-09-01 02:22:47 -0600

    Ensure ecomplete database is loaded; refactor

--- my-lisp/ecomplete-extras.el
+++ my-lisp/ecomplete-extras.el
@@ -10,6 +10,11 @@
             email (match-string 2 email)))
     (cons name email)))
 
+(defun ecomplete--read-address ()
+  "Read an email address with completion."
+  (unless ecomplete-database (ecomplete-setup))
+  (completing-read "Email address: " (ecomplete-completion-table 'mail)))
+
 (defun add-email-to-ecomplete (email)
   "Add email address to ecomplete's database."
   (interactive "sEmail address: ")
@@ -26,9 +31,7 @@
 
 (defun remove-email-from-ecomplete (email)
   "Remove email address from ecomplete's database."
-  (interactive
-   (list (completing-read "Email address: "
-                          (ecomplete-completion-table 'mail))))
+  (interactive (list (ecomplete--read-address)))
   (when-let ((email (cdr (email--name+address email))) 
              (entry (ecomplete-get-item 'mail email)))
     (setf (cdr (assq 'mail ecomplete-database))
@@ -37,9 +40,7 @@
 
 (defun compose-mail-to (address)
   "Compose email to ADDRESS from ecomplete's database."
-  (interactive
-   (list (completing-read "Email address: "
-                          (ecomplete-completion-table 'mail))))
+  (interactive (list (ecomplete--read-address)))
   (compose-mail address))
 
 (provide 'ecomplete-extras)
