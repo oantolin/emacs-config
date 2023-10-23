cbcd6c34129a08a127c4ac3bcb279bcaa022f314
Omar Antolín Camarena <omar.antolin@gmail.com>
2023-10-23 16:57:29 -0600

    Install lingva for translations

--- init.el
+++ init.el
@@ -312,6 +312,13 @@
 (use-package topaz-paste
   :commands topaz-paste-region topaz-paste-buffer)
 
+(use-package lingva
+  :ensure t
+  :bind
+  ("C-c l" . lingva-translate)
+  :custom
+  (lingva-instance "https://lingva.lunar.icu"))
+
 (use-package isearch-extras
   :custom
   (search-whitespace-regexp ".*?")
