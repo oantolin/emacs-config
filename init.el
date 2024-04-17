4e2bab292c8e71ba06513cdc019dbaecee82bc35
Omar Antolín Camarena <omar.antolin@gmail.com>
2024-04-17 15:54:30 -0600

    Spring is over, now it's summer again
    I think I like the pink ef-themes better.

--- init.el
+++ init.el
@@ -204,12 +204,12 @@
   :bind
   ("C-c x b" . ef-themes-toggle)
   :custom
-  (ef-themes-to-toggle '(ef-spring ef-bio))
+  (ef-themes-to-toggle '(ef-summer ef-winter))
   (ef-themes-variable-pitch-ui t)
   (ef-themes-mixed-fonts t)
   (ef-themes-headings '((0 1.4) (1 1.3) (2 1.2) (3 1.1)))
   :init
-  (load-theme (if (display-graphic-p) 'ef-spring 'ef-bio) t))
+  (load-theme (if (display-graphic-p) 'ef-summer 'ef-winter) t))
 
 (use-package spacious-padding
   :ensure t
