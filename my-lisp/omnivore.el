5bd00f4bfb3167e0914df51bc598338705cde748
Omar Antolín Camarena <omar.antolin@gmail.com>
2024-07-23 15:41:08 -0600

    Use API key for Omnivore
    For some reason authenticating with the browser cookie didn't work on
    my Windows machine, so I switched to using POST with an API key. This
    is much more complicated, but I expect it'll work everywhere. 🤷

--- my-lisp/omnivore.el
+++ my-lisp/omnivore.el
@@ -1,12 +1,61 @@
 ;;; omnivore.el --- Add URLs to Omnivore's reading list  -*- lexical-binding: t; -*-
 
+;; See https://docs.omnivore.app/integrations/api.html
+(defconst omnivore--save-query
+  "mutation SaveUrl($input: SaveUrlInput!) { saveUrl(input: $input) { ... on SaveSuccess { url clientRequestId } ... on SaveError { errorCodes message } } }"
+  "Query used to save a URL to Omnivore's reading list.")
+
+(defun omnivore--uuid ()
+  "Return string with random (version 4) UUID.
+This function is the same as `org-id-uuid', duplicated here to
+avoid having to load Org."
+  (let ((rnd (md5 (format "%s%s%s%s%s%s%s"
+			  (random)
+			  (time-convert nil 'list)
+			  (user-uid)
+			  (emacs-pid)
+			  (user-full-name)
+			  user-mail-address
+			  (recent-keys)))))
+    (format "%s-%s-4%s-%s%s-%s"
+	    (substring rnd 0 8)
+	    (substring rnd 8 12)
+	    (substring rnd 13 16)
+	    (format "%x"
+		    (logior
+		     #b10000000
+		     (logand
+		      #b10111111
+		      (string-to-number
+		       (substring rnd 16 18) 16))))
+	    (substring rnd 18 20)
+	    (substring rnd 20 32))))
+
+(defun omnivore--api-key ()
+  "Return Omnivore API key stored in authinfo."
+  (if-let ((token (car (auth-source-search :max 1 :host "omnivore.app"))))
+      (auth-info-password token)
+    (user-error "No Omnivore API key found")))
+  
 (defun omnivore-add-url (url)
   "Add URL to Omnivore's reading list.
 For simplicity, this uses your browser cookie to authenticate."
   (interactive "sURL: ")
-  (browse-url-default-browser
-   (concat "https://omnivore.app/api/save?url="
-           (browse-url-encode-url url))))
+  (let ((url-request-method "POST")
+        (url-request-extra-headers
+         `(("content-type" . "application/json")
+           ("authorization" . ,(omnivore--api-key))))
+        (url-request-data
+         (json-encode
+          `((query . ,omnivore--save-query)
+            (variables
+             (input
+              (clientRequestId . ,(omnivore--uuid))
+              (source . api)
+              (url . ,url)))))))
+    (url-retrieve
+     "https://api-prod.omnivore.app/api/graphql"
+     (lambda (_) (message "Added %s" url)))))
 
 (defun omnivore-add-kill ()
   "Add most recent kill to Omnivore's reading list."
@@ -20,7 +69,7 @@ For simplicity, this uses your browser cookie to authenticate."
   (interactive)
   (if-let ((url (eww-current-url)))
       (omnivore-add-url url)
-    (user-error "Not a eww buffer.")))
+    (user-error "Not a eww buffer")))
 
 (defun omnivore-add-shr-url ()
   "Add URL of shr link at point to Omnivore's reading list."
