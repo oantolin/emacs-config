9fe9d1a819b0a7421512832d1bdd3b79bc129d8e
Omar Antolin Camarena <omar@oficina-omar.matem.unam.mx>
2025-02-25 10:53:49 -0600

    Revert "Looks like vc-git-commit is no longer necessary!"
    This reverts commit ffb506edd11638b767b4130fef55ab47cd22bedb.

--- my-lisp/vc-extras.el
+++ my-lisp/vc-extras.el
@@ -18,4 +18,17 @@ Intended to be used as advice for `consult-history'."
     (kill-new (format "%s" commit))
     (message "Copied: %s" commit)))
 
+(defun vc-git-commit (message)
+  "Run git commit -m MESSAGE.
+Interactively MESSAGE is just \"Merge resolving conflicts\", but
+with a prefix argument you are prompted for a message."
+  (interactive
+   (list (if current-prefix-arg
+             (read-from-minibuffer "Commit message: " "Merge")
+           "Merge resolving conflicts")))
+  (vc-git-command nil 0 nil "commit" "-m" message)
+  (message "Commited with message: %s" message)
+  (when (derived-mode-p 'vc-dir-mode)
+    (revert-buffer)))
+
 (provide 'vc-extras)
