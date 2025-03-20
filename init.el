cc0fb7e22b5fe59c8db4b13bcfcc85103fba5e7c
Omar Antolin Camarena <omar@oficina-omar.matem.unam.mx>
2025-03-20 13:24:33 -0600

    Simplify displaying of gptel-mini response
    I don't really care if it is a side window or not. Most of my popups
    use a simple pop-to-buffer. If I want this to be in a side window I'll
    configure display-buffer-alist.

--- init.el
+++ init.el
@@ -358,32 +358,26 @@
     "Display the LLM's response to PROMPT.
 If the region is active, it is included as context. If the response is
 short, it is shown in the echo area; otherwise, it is displayed in a
-side window."
+buffer."
     (interactive "sAsk LLM: ")
     (when (string= prompt "") (user-error "A prompt is required."))
     (when (use-region-p)
       (gptel-context--add-region
        (current-buffer) (region-beginning) (region-end)))
     (gptel-request
-        prompt
-      :callback
-      (lambda (response info)
-        (cond
-         ((not response)
-          (message "gptel-mini failed with message: %s"
-                   (plist-get info :status)))
-         ((< (length response) 250)
-          (message "%s" response))
-         (t (with-current-buffer (get-buffer-create "*gptel-mini*")
-              (let ((inhibit-read-only t))
-                (erase-buffer)
-                (insert response))
-              (special-mode)
-              (display-buffer
-               (current-buffer)
-               `((display-buffer-in-side-window)
-                 (side . bottom)
-                 (window-height . ,#'fit-window-to-buffer))))))))))
+     prompt
+     :callback
+     (lambda (response info)
+       (cond ((not response)
+              (message "gptel-mini failed with message: %s"
+                       (plist-get info :status)))
+             ((< (length response) 250)
+              (message "%s" response))
+             (t (pop-to-buffer (get-buffer-create "*gptel-mini*"))
+                (let ((inhibit-read-only t))
+                  (erase-buffer)
+                  (insert response))
+                (special-mode)))))))
   
 (use-package isearch-extras
   :custom
