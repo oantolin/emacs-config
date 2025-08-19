07360335fb3c1cd1a0275fce3fb7ae7acd2a9474
Omar Antolín <omar.antolin@gmail.com>
2025-08-18 22:33:38 -0600

    Whitespace

--- my-lisp/man-help.el
+++ my-lisp/man-help.el
@@ -1,6 +1,5 @@
 ;;; man-help.el --- Use man for help in shell command prompts    -*- lexical-binding: t; -*-
 
-
 (define-advice read-shell-command
     (:around (fn &rest args) use-man-for-local-help)
   "Remap `display-local-help' to `man' in the minibuffer.
