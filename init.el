b515a63498be00197bdf78ac46cea69040c48dd6
Omar Antolín <omar.antolin@gmail.com>
2026-01-20 17:02:10 -0600

    Add binding for pdf-view-roll-minor-mode

--- init.el
+++ init.el
@@ -878,6 +878,7 @@
         ("i" . consult-imenu)
         ("t" . pdf-view-themed-minor-mode)
         ("d" . pdf-view-midnight-minor-mode)
+        ("c" . pdf-view-roll-minor-mode)
         ("s n" . "nsbp")
         ([remap scroll-up-command] . pdf-view-scroll-up-or-next-page)
         ([remap scroll-down-command] . pdf-view-scroll-down-or-previous-page))
