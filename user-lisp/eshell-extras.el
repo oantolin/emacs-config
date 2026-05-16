37f5171fc12243ae18332c57687dbc43dedcd68a
Omar Antolín <omar.antolin@gmail.com>
2026-05-16 12:37:05 -0600

    Oops, correct use of declare-function, require some libraries instead

--- user-lisp/eshell-extras.el
+++ user-lisp/eshell-extras.el
@@ -1,7 +1,7 @@
 ;;  eshell-extras.el --- Miscellaneous eshell commands   -*- lexical-binding: t; -*-
 
-(declare-function 'eshell-send-input "esh-mode")
-(declare-function 'term-send-input "term")
+(require 'term)
+(require 'esh-mode)
 
 (defun interactive-cd (dir)
   "Prompt for a directory and cd to it."
@@ -27,7 +27,7 @@
   (term-mode)
   (term-char-mode))
 
-(declare-function 'bookmark-prop-get "bookmark")
+(declare-function bookmark-prop-get "bookmark")
 
 ;; The built-in version of the following function doesn't ensure you
 ;; get an eshell buffer in the right directory!
